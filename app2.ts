afa6dd74a41047848b27042f49942b28346b874c
a-tione <heyanggo@gmail.com>
2020-10-20 21:36:07 +0800

    add typescript hello world

---
+++ app2.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/xx', (req, res) => res.send('你好'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
